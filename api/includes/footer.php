904d550adf5c657ff61884a6225d18999073790c
swarajdhore <65715910+swarajdhore@users.noreply.github.com>
2021-08-05 10:17:07 +0530

    Add files via upload

---
+++ api/includes/footer.php
@@ -0,0 +1,24 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+              <h3>Information</h3>
+                <p><a href="about.php"><span style="color:white">About Us</span></a></p>
+                <p><a href="contact.php"><span style="color:white">Contact Us</span></a></p>
+            </div>
+
+            <div class="col-md-4">
+              <h3>My Account</h3>
+             <p><a href="#" data-toggle="modal" data-target="#loginmodal" ><span style="color:white">Log In</span></a></p>
+             <p><a href="signup.php"><span style="color:white">Sign Up</span></a></p>
+             </div>
+
+            <div class="col-md-4">
+              <h3>Contact Us</h3>
+                <p>CONTACT No. +91-123-000000</p>
+                
+            </div>
+        </div>
+    <a href="https://info.flagcounter.com/nNdZ"><img src="https://s01.flagcounter.com/count2/nNdZ/bg_FFFFFF/txt_000000/border_CCCCCC/columns_2/maxflags_10/viewers_0/labels_1/pageviews_1/flags_0/percent_0/" alt="Flag Counter" border="0"></a>    
+    </div>
+</footer>
